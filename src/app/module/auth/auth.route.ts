f0478f05c9c832c236215202d89b6433974da859
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-02-11 08:07:34 +0600

    Login User With Email Password

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,39 +1,9 @@
-import { Request, Response } from "express";
-import { catchAsync } from "../../shared/catchAsync";
-import { sendResponse } from "../../shared/sendResponse";
-import { AuthService } from "./auth.service";
+import { Router } from "express";
+import { AuthController } from "./auth.controller";
 
-const registerPatient = catchAsync(
-    async (req: Request, res: Response) => {
-        const payload = req.body;
+const router = Router()
 
-        console.log(payload);
+router.post("/register", AuthController.registerPatient)
+router.post("/login", AuthController.loginUser)
 
-        const result = await AuthService.registerPatient(payload);
-
-        sendResponse(res, {
-            httpStatusCode: 201,
-            success: true,
-            message: "Patient registered successfully",
-            data: result,
-        })
-    }
-)
-
-const loginUser = catchAsync(
-    async (req: Request, res: Response) => {
-        const payload = req.body;
-        const result = await AuthService.loginUser(payload);
-        sendResponse(res, {
-            httpStatusCode: 200,
-            success: true,
-            message: "User logged in successfully",
-            data: result,
-        })
-    }
-)
-
-export const AuthController = {
-    registerPatient,
-    loginUser,
-};
+export const AuthRoutes = router;
