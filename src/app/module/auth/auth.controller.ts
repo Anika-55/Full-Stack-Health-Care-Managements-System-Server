fef4f4bbb91ecd576e6bc40e982cb18bbc47cf97
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-02-13 19:32:28 +0600

    Completing Create Doctor And Doctor Specialty Model And Route, Controller, Service Business Logic With Transaction In User Service

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sendResponse";
 import { AuthService } from "./auth.service";
+import status from "http-status";
 
 const registerPatient = catchAsync(
     async (req: Request, res: Response) => {
@@ -12,7 +13,7 @@ const registerPatient = catchAsync(
         const result = await AuthService.registerPatient(payload);
 
         sendResponse(res, {
-            httpStatusCode: 201,
+            httpStatusCode: status.OK,
             success: true,
             message: "Patient registered successfully",
             data: result,
