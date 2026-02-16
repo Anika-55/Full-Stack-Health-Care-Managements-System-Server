1a615cfdd9da73cb9af0a820c2d6b4ac7db0eb3d
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-02-16 10:00:27 +0600

    Create Custom Error Class And Handle Native Errors Of Javascript

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
-import { prisma } from "./app/lib/prisma";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 import { IndexRoutes } from "./app/routes";
-
 
 const app: Application = express();
 
@@ -11,20 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use("/api/v1", IndexRoutes)
+app.use(cookieParser())
 
+app.use("/api/v1", IndexRoutes);
+
+// Basic route
 app.get('/', async (req: Request, res: Response) => {
-    
-    const specialty = await prisma.specialty.create({
-        data: {
-            title:"Cardiology"
-        }
-    })
-    
     res.status(201).json({
         success: true,
-        message: "api is working",
-        data:specialty
+        message: 'API is working',
     })
-})
+});
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
+
 export default app;
